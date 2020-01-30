9c0fea173b5bd54672abf7d940de37e40d9eda40
Alexander Hjelm <nick.nertova@gmail.com>
2020-01-30 18:18:53 +0100

    GeoJson importer routine at Awake

---
+++ unity-project/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GeoJSON;
+
+public class GameManager : MonoBehaviour
+{
+    private string[] _highwayCategories = new string[]
+    {
+        "footpath",
+        "residential",
+        "secondary",
+        "primary"
+    };
+
+    void Start()
+    {
+        foreach(string hwy in _highwayCategories)
+        {
+            FeatureCollection collection = GeoJSONImporter.ReadFeatureCollectionFromFile("MapData/" + hwy);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ unity-project/Assets/Scripts/GeoJSONImporter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using GeoJSON;
+
+public class GeoJSONImporter
+{
+    // Start is called before the first frame update
+    public static FeatureCollection ReadFeatureCollectionFromFile(string path)
+    {
+        TextAsset encodedGeoJSON = Resources.Load(path) as TextAsset;
+        FeatureCollection collection = GeoJSON.GeoJSONObject.Deserialize(encodedGeoJSON.text);
+        return collection;
+    }
+
+}

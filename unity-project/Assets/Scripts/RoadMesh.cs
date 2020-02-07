8eef4f1f29c53106561faedd1585d4d6e0b94342
Alexander Hjelm <nick.nertova@gmail.com>
2020-02-07 14:42:06 +0100

    Smooth road segment transitions in the mesh

--- unity-project/Assets/Scripts/RoadMesh.cs
+++ unity-project/Assets/Scripts/RoadMesh.cs
@@ -5,6 +5,20 @@ using UnityEngine;
 
 public class RoadMesh : MonoBehaviour
 {
+    private struct RoadEndPoint
+    {
+        public Vector3 leftPoint;
+        public Vector3 rightPoint;
+        public Vector3 tangent;
+
+        public RoadEndPoint(Vector3 leftPoint, Vector3 rightPoint, Vector3 tangent)
+        {
+            this.leftPoint = leftPoint;
+            this.rightPoint = rightPoint;
+            this.tangent = tangent;
+        }
+    }
+
     private MeshFilter _meshFilter;
     private float _scale = 50f;
     private Vector2 _offset = new Vector2(-18.05f, -59.34f);
@@ -27,18 +41,49 @@ public class RoadMesh : MonoBehaviour
         int countedVertices = 0;
         foreach(List<RoadNode> path in paths)
         {
+            RoadEndPoint previousRoadEndPoint = new RoadEndPoint(Vector3.zero, Vector3.zero, Vector3.zero);
+
             for(int i=0; i<path.Count-1; i++)
             {
                 RoadNode a = path[i];
                 RoadNode b = path[i+1];
                 Vector3 posA = TransformPointToMeshSpace(a.GetPosAsVector2());
                 Vector3 posB = TransformPointToMeshSpace(b.GetPosAsVector2());
-                Vector3 tangent = posB - posA;
-                Vector3 normal = new Vector3(tangent.z, 0f, -tangent.x).normalized*0.001f;
-                vertices.Add(posA + normal*roadWidth);
-                vertices.Add(posA - normal*roadWidth);
-                vertices.Add(posB + normal*roadWidth);
-                vertices.Add(posB - normal*roadWidth);
+                Vector3 tangent = (posB - posA).normalized;
+                Vector3 normal = new Vector3(tangent.z, 0f, -tangent.x)*0.001f;
+
+                Vector3 leftStartPoint = posA + normal*roadWidth;
+                Vector3 rightStartPoint = posA - normal*roadWidth;
+                Vector3 leftEndPoint = posB + normal*roadWidth;
+                Vector3 rightEndPoint = posB - normal*roadWidth;
+
+                // If this is not the first path segment, merge it with the previous one
+                if(previousRoadEndPoint.tangent != Vector3.zero)
+                {
+                    // Update start points of this segment and end points of the last segment
+                    // TODO: Right now there are duplicate vertices. Find a way to count vertices so that they merge instead
+                    Vector3 intersectionLeft;
+                    Vector3 intersectionRight;
+                    if(MathUtils.LineLineIntersection(out intersectionLeft, leftStartPoint, tangent,
+                        previousRoadEndPoint.leftPoint, previousRoadEndPoint.tangent))
+                    {
+                        leftStartPoint = intersectionLeft;
+                        // TODO: List access here may be slow, find a faster way of updating
+                        vertices[vertices.Count-2] = intersectionLeft;
+                    }
+                    if(MathUtils.LineLineIntersection(out intersectionRight, rightStartPoint, tangent,
+                        previousRoadEndPoint.rightPoint, previousRoadEndPoint.tangent))
+                    {
+                        rightStartPoint = intersectionRight;
+                        vertices[vertices.Count-1] = intersectionRight;
+                    }
+                }
+
+                vertices.Add(leftStartPoint);
+                vertices.Add(rightStartPoint);
+                vertices.Add(leftEndPoint);
+                vertices.Add(rightEndPoint);
+
                 triangles.Add(countedVertices);
                 triangles.Add(countedVertices+1);
                 triangles.Add(countedVertices+2);
@@ -46,6 +91,9 @@ public class RoadMesh : MonoBehaviour
                 triangles.Add(countedVertices+2);
                 triangles.Add(countedVertices+1);
                 countedVertices += 4;
+
+                // Set the last end point for the next path segment
+                previousRoadEndPoint = new RoadEndPoint(leftEndPoint, rightEndPoint, tangent);
             }
         }
 

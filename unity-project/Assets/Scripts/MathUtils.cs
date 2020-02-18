4ff4566b96b1019a845345cf6f707a038c11731e
Alexander Hjelm <nick.nertova@gmail.com>
2020-02-18 19:16:59 +0100

    Add Line-Plane intersection function to math utils

--- unity-project/Assets/Scripts/MathUtils.cs
+++ unity-project/Assets/Scripts/MathUtils.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class MathUtils : MonoBehaviour
 {
+    // More here: http://wiki.unity3d.com/index.php/3d_Math_functions?_ga=2.226594461.1045422346.1582048800-1214154379.1582048800
+
+    //create a vector of direction "vector" with length "size"
+	public static Vector3 SetVectorLength(Vector3 vector, float size){
+ 
+		//normalize the vector
+		Vector3 vectorNormalized = Vector3.Normalize(vector);
+ 
+		//scale the vector
+		return vectorNormalized *= size;
+	}
+
     //Calculate the intersection point of two lines. Returns true if lines intersect, otherwise false.
 	//Note that in 3d, two lines do not intersect most of the time. So if the two lines are not in the 
 	//same plane, use ClosestPointsOnTwoLines() instead.
@@ -25,6 +37,39 @@ public class MathUtils : MonoBehaviour
 		else
 		{
 			intersection = Vector3.zero;
+			return false;
+		}
+	}
+
+    //Get the intersection between a line and a plane. 
+	//If the line and plane are not parallel, the function outputs true, otherwise false.
+	public static bool LinePlaneIntersection(out Vector3 intersection, Vector3 linePoint, Vector3 lineVec, Vector3 planeNormal, Vector3 planePoint){
+ 
+		float length;
+		float dotNumerator;
+		float dotDenominator;
+		Vector3 vector;
+		intersection = Vector3.zero;
+ 
+		//calculate the distance between the linePoint and the line-plane intersection point
+		dotNumerator = Vector3.Dot((planePoint - linePoint), planeNormal);
+		dotDenominator = Vector3.Dot(lineVec, planeNormal);
+ 
+		//line and plane are not parallel
+		if(dotDenominator != 0.0f){
+			length =  dotNumerator / dotDenominator;
+ 
+			//create a vector from the linePoint to the intersection point
+			vector = SetVectorLength(lineVec, length);
+ 
+			//get the coordinates of the line-plane intersection point
+			intersection = linePoint + vector;	
+ 
+			return true;	
+		}
+ 
+		//output not valid
+		else{
 			return false;
 		}
 	}

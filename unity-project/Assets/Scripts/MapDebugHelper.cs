36b7c3ddd42a74fa4cba9f341b795cf5d24a1716
Alexander Hjelm <nick.nertova@gmail.com>
2020-02-22 20:09:56 +0100

    Fix oops in MapDebugHelper

--- unity-project/Assets/Scripts/MapDebugHelper.cs
+++ unity-project/Assets/Scripts/MapDebugHelper.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class MapDebugHelper : MonoBehaviour
 {
-    private static float targetNodeX = 6.144524;
-    private static float targetNodeY = -7.995605;
+    private static double targetNodeX = 6.144524;
+    private static double targetNodeY = -7.995605;
 
     public static void ConditionalNodeLog(RoadNode node, string msg)
     {
-        ConditionalNodeLog(new Vector2(node.GetX(), node.GetY()));
+        ConditionalNodeLog(new Vector2(node.GetX(), node.GetY()), msg);
     }
 
     public static void ConditionalNodeLog(Vector3 point, string msg)
     {
-        ConditionalNodeLog(new Vector2(point.x, point.z));
+        ConditionalNodeLog(new Vector2(point.x, point.z), msg);
     }
 
     public static void ConditionalNodeLog(Vector2 point, string msg)

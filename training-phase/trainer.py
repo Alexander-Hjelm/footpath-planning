66179c31121cd16fb87bc06765b00ba7f4c14fec
Alexander Hjelm <nick.nertova@gmail.com>
2020-02-25 14:23:19 +0100

    Implement patch extraction algorithm according to Nishida et al

--- training-phase/trainer.py
+++ training-phase/trainer.py
@@ -1,4 +1,6 @@
 from geojson import Point, Feature, FeatureCollection, dump, load
+import math
+from queue import Queue
 
 data_dir = "../osm-importer/raw_data/"
 files_to_read = [
@@ -7,6 +9,7 @@ files_to_read = [
         "secondary.geojson",
         "primary.geojson"
         ]
+vertex_distance_threshold = 0.0002
 
 class Node:
     x = 0.0
@@ -16,6 +19,65 @@ class Node:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def distance_to(self, u):
+        return math.sqrt((self.x-u.x)**2 + (self.y-u.y)**2)
+
+    def add_neighbour(self, u):
+        if not(u in self.neighbours):
+            if not(u == self):
+                self.neighbours.append(u)
+
+class Patch:
+    vertices = []
+
+    #TODO: type checking in methods
+    def __init__(self, vertices):
+        self.vertices = vertices
+
+    def add_vertex(self, vertex):
+        self.vertices.append(vertex)
+
+    def add_vertices(self, vertices_in):
+        for vertex in vertices_in:
+            self.add_vertex(vertex)
+
+def Detect(G):
+    return []
+
+def Expand(patch):
+    # Vertex processing queue
+    Q = Queue()
+    processed_vertices = []
+
+    for vertex in patch.vertices:
+        Q.put(vertex)
+        processed_vertices.append(vertex)
+    while Q.empty():
+        v = Q.get()
+        for u in v.neighbours:
+            if(not u == v):
+                #print(v.distance_to(u))
+                if not u in processed_vertices:
+                    if v.distance_to(u) < vertex_distance_threshold:
+                        patch.add_vertex(u)
+                        Q.put(u)
+                        processed_vertices.append(u)
+
+def IsVertexInAnyPatch(vertex, patches):
+    for patch in patches:
+        if vertex in patch:
+            return True
+    return False
+
+def ExtractPatches(G):
+    patches = Detect(G)
+    for patch in patches:
+        Expand(patch)
+    for vertex in G:
+        if not IsVertexInAnyPatch(vertex, patches):
+            patch = Patch([vertex])
+            Expand(patch)
 
 for file_name in files_to_read:
 
@@ -51,5 +113,14 @@ for file_name in files_to_read:
             if i > 0:
                 x_prev = coordinates[i-1][0]
                 y_prev = coordinates[i-1][1]
-                node.neighbours.append(nodes_by_coord[x_prev][y_prev])
-                nodes_by_coord[x_prev][y_prev].neighbours.append(node)
+                if(node == nodes_by_coord[x_prev][y_prev]):
+                    print("Error: Added the node as its own neighbour")
+                node.add_neighbour(nodes_by_coord[x_prev][y_prev])
+                nodes_by_coord[x_prev][y_prev].add_neighbour(node)
+
+    G = []
+    for x in nodes_by_coord:
+        for y in nodes_by_coord[x]:
+            G.append(nodes_by_coord[x][y])
+
+    ExtractPatches(G)

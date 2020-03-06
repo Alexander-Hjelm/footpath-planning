2db874b5a8f870933bd56e80149fdbd014c58b0f
Alexander Hjelm <nick.nertova@gmail.com>
2020-03-06 14:22:28 +0100

    Add BuildingFootprint data class

---
+++ unity-project/Assets/Scripts/BuildingFootprint.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingFootprint
+{
+    private List<Vector3> _vertices = new List<Vector3>();
+
+    public void AddVertex(Vector3 vertex)
+    {
+        _vertices.Add(vertex);
+    }
+
+    public List<Vector3> GetVertices()
+    {
+        return _vertices;
+    }
+}

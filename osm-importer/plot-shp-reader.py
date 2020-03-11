94fc3d6010901ba8125b992c349b51bc8879da30
Alexander Hjelm <nick.nertova@gmail.com>
2020-03-11 12:39:23 +0100

    Add routine for coordinate conversion for plot data

--- osm-importer/plot-shp-reader.py
+++ osm-importer/plot-shp-reader.py
@@ -7,9 +7,33 @@ print(shapes.schema)
 #first = shapes.next()
 data_out = []
 
+def epsg3006_to_wgs84(point):
+    x = point[0]
+    y = point[1]
+    # Coordinate conversion from EPSG:3006 to WGS:84
+    x_new = 0.000011199*x + 10.5112561156
+    y_new = 0.000009016*y
+    return (x_new, y_new)
+
+
 for i in range(0, len(shapes)):
-    data_out.append(shapes[i])
-    #print(shapes[i]) # (GeoJSON format)
+    shape = shapes[i]
+    coordinates = shape['geometry']['coordinates'][0]
+
+    # Coordinate conversion
+    for j in range(0, len(coordinates)):
+        point = coordinates[j]
+        if type(point) == tuple:
+            coordinates[j] = epsg3006_to_wgs84(point)
+        elif type(point) == list:
+            for k in range(0, len(point)):
+                coordinates[j][k] = epsg3006_to_wgs84(coordinates[j][k])
+            pass
+        else:
+            print("ERROR: No handling for points of type: " + type(point))
+
+    # Append shape to output data in GeoJSON format
+    data_out.append(shape)
 
 with open('raw_data/plots.geojson', 'w') as f:
     dump(FeatureCollection(data_out), f)

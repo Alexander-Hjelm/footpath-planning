7e481c33c3a1927c2a91c7b6c612cbe19664f11b
Alexander Hjelm <nick.nertova@gmail.com>
2020-03-19 13:48:44 +0100

    Move coordinate ordering to a new script, that also does projection

--- osm-importer/building-metrics.py
+++ osm-importer/building-metrics.py
@@ -1,7 +1,6 @@
 # This script reads processed OSM and SLU building data, and finds the metrics specified in the project report
 # REQUIREMENTS:
-# - Run building-data-formatter.py
-# - Run plot-shp-reader.py
+# - Run convert-to-sweref.py
 
 from geojson import Point, Feature, FeatureCollection, load
 
@@ -17,26 +16,6 @@ def polygon_area(vertices):
     result = a / 2.0
     return result
 
-def get_vertices_of_feature(feature):
-    coordinates = feature['geometry']['coordinates']
-    vertices_out = []
-    for point in coordinates:
-        if type(point) == tuple:
-            vertices_out.append(coordinates[point])
-        elif type(point) == list:
-            for k in range(0, len(point)):
-                if type(point) == tuple:
-                    vertices_out.append(point[k])
-                elif type(point[k]) == list:
-                    if len(point[k]) == 2:
-                        vertices_out.append((point[k][0], point[k][1]))
-                    else:
-                        if type(point[k]) == list:
-                            for l in range(0, len(point[k])):
-                                vertices_out.append((point[k][l][0], point[k][l][1]))
-
-    return vertices_out
-
 # read files
 with open('raw_data/buildings.geojson', 'r') as f:
     OSM_data = load(f)
@@ -49,8 +28,8 @@ print("Finished loading map data")
 total_area_OSM = 0.0
 total_area_SLU = 0.0
 for feature in OSM_data['features']:
-    total_area_OSM += polygon_area(get_vertices_of_feature(feature))
+    total_area_OSM += polygon_area(feature['geometry']['coordinates'])
 for feature in SLU_data['features']:
-    total_area_SLU += polygon_area(get_vertices_of_feature(feature))
+    total_area_SLU += polygon_area(feature['geometry']['coordinates'])
 print(total_area_OSM)
 print(total_area_SLU)

82d7808b0eb12e336669033e4297cfa7621ee3b8
Alexander Hjelm <nick.nertova@gmail.com>
2020-03-19 14:09:44 +0100

    Fix shoelace formula

--- osm-importer/building-metrics.py
+++ osm-importer/building-metrics.py
@@ -12,8 +12,9 @@ def polygon_area(vertices):
     a = 0.0
     for i in range(n):
         j = (i + 1) % n
-        a += abs(vertices[i][0] * vertices[j][1]-vertices[j][0] * vertices[i][1])
-    result = a / 2.0
+        a += vertices[i][0] * vertices[j][1]
+        a -= vertices[j][0] * vertices[i][1]
+    result = abs(a) / 2.0
     return result
 
 # read files

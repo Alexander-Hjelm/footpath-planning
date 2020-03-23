43795b7fdf1b11a0a92d3996830254a0c7e63d75
Alexander Hjelm <nick.nertova@gmail.com>
2020-03-23 12:32:22 +0100

    Set bbox padding to 0.005

--- osm-importer/building-cropper.py
+++ osm-importer/building-cropper.py
@@ -12,7 +12,7 @@ q_bbox_n = 59.34469860821763
 q_bbox_e = 18.066052311607727
 q_bbox_s = 59.328128796834925
 q_bbox_w = 18.03809503228281
-q_p = 0.001 # Padding for query bbox
+q_p = 0.005 # Padding for query bbox
 
 q_bbox_n = q_bbox_n - q_p
 q_bbox_e = q_bbox_e - q_p

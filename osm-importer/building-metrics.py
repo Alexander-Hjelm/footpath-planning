6def188e3a81d6f9a6388c127b3a2b0d77a5c72c
Alexander Hjelm <nick.nertova@gmail.com>
2020-03-23 17:37:54 +0100

    Building metrics script: Feature mapping

--- osm-importer/building-metrics.py
+++ osm-importer/building-metrics.py
@@ -3,19 +3,25 @@
 # - Run buildings-convert-to-sweref.py
 
 from geojson import Point, Feature, FeatureCollection, load
+import shapely.geometry
 
 OSM_data = {}
 SLU_data = {}
 
-def polygon_area(vertices):
-    n = len(vertices) # of corners
-    a = 0.0
-    for i in range(n):
-        j = (i + 1) % n
-        a += vertices[i][0] * vertices[j][1]
-        a -= vertices[j][0] * vertices[i][1]
-    result = abs(a) / 2.0
-    return result
+def polygon_intersection_area(polygon_1, polygon_2):
+    shapely_poly_1 = shapely.geometry.Polygon(polygon_1)
+    shapely_poly_2 = shapely.geometry.Polygon(polygon_2)
+    return shapely_poly_1.intersection(shapely_poly_2).area
+
+def polygon_area(polygon):
+    shapely_poly = shapely.geometry.Polygon(polygon)
+    return shapely_poly.area
+
+def polygon_relative_overlap(polygon_1, polygon_2):
+    area_overlap = polygon_intersection_area(polygon_1, polygon_2)
+    area_1 = polygon_area(polygon_1)
+    area_2 = polygon_area(polygon_2)
+    return area_overlap / min(area_1, area_2)
 
 def add_areas_recursively(c):
     area_out = 0.0
@@ -35,6 +41,20 @@ def add_areas_recursively(c):
                 area_out += add_areas_recursively(c_sub)
     return area_out
 
+def extract_polygon_from_feature(feature):
+    feature_type = feature['geometry']['type']
+
+    if feature_type == 'Polygon':
+        # Even for multipolygons, the big surrounding polygon is always the first one
+        return feature['geometry']['coordinates'][0]
+
+    if feature_type == 'MultiPolygon':
+        #TODO: Return the very first polygon for now. Is this correct?
+        return feature['geometry']['coordinates'][0][0]
+
+    print("extract_polygon_from_feature: feature type not implemented: " + feature_type)
+    print(feature)
+
 # read files
 with open('raw_data/buildings-osm-sweref.geojson', 'r') as f:
     OSM_data = load(f)
@@ -52,3 +72,27 @@ for feature in SLU_data['features']:
     total_area_SLU += add_areas_recursively(feature['geometry']['coordinates'])
 print("Total area, OSM: " + str(total_area_OSM))
 print("Total area, SLU: " + str(total_area_SLU))
+print("Total area, fraction: " + str(total_area_OSM / total_area_SLU))
+
+# Feature mapping
+overlapping_buildings = {}
+progress = 0.0
+for feature_osm in OSM_data['features']:
+    print("Mapping features, progess: " + str(int(100*progress/len(OSM_data['features']))) + '%')
+    progress+=1.0
+
+    overlapping_buildings[feature_osm['id']] = []
+
+    # Build polygon and match with SLU polygons
+    polygon_osm = extract_polygon_from_feature(feature_osm)
+    for feature_slu in SLU_data['features']:
+        polygon_slu = extract_polygon_from_feature(feature_slu)
+
+        relative_overlap = polygon_relative_overlap(polygon_osm, polygon_slu)
+        if relative_overlap > 0.3:
+            overlapping_buildings[feature_osm['id']].append(feature_slu)
+
+#TODO: Store overlapping buildings as id -> featurelist
+#TODO: A limit on the number of buildings counted
+#TODO: Shout if the area in SLU is bigger
+#TODO: Bounding polygon method

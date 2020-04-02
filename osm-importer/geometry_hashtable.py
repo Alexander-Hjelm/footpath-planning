54b807e0756ed5bdaf5a19c3e73b96b88bbc1f77
Alexander Hjelm <nick.nertova@gmail.com>
2020-04-02 14:41:37 +0200

    Initial implementation of geometrical hashtable

--- osm-importer/geometry_hashtable.py
+++ osm-importer/geometry_hashtable.py
@@ -1,38 +1,58 @@
 import pickle
+import math
+import geometry_utils
 
 class GeometryHashtable:
 
     name = None
     hashtable = None
+    cell_size = None
+    top_left_x = None
+    top_left_y = None
 
-    def __init(self, name):
-        # Load a hashtable from file
+    def __init__(self, name, cell_size):
         self.name = name
-        _load_hashtable_from_file()
+        self.cell_size = cell_size
 
-    def __init__(self, name, polygons, cell_size, top_left_x, top_left_y):
-        # Create a hashtable from a list of polygons and save it to file
-        self.name = name
-        self.hashtable = self._create_hashtable_from_polygons(polygons)
-        _write_hashtable_to_file()
+    def create_from_polygons_list(self, polygons):
+        min_x, min_y, max_x, max_y = geometry_utils.minmax_points_of_polygons(polygons)
+        x_d = max_x - min_x
+        y_d = max_y - min_y
+        cell_count_x = math.ceil(x_d/cell_size)
+        cell_count_y = meah.ceil(y_d/cell_size)
+        hashtable = []
+        for i in range(0, cell_count_x):
+            hashtable.append([])
+            for j in range(0, cell_count_y):
+                hashtable[i].append([])
 
-    def get_polygons_in_bucket_surrounding(x, y):
-        pass
+        for polygon in polygons:
+            x, y = get_hash_keys_of_polygon(polygon)
+            hashtable[x][y] = polygon
 
-    def get_polygons_in_bucket(x, y):
-        pass
+        self.hashtable = hashtable
 
-    def get_hash_keys_of_point(x, y):
-        pass
+    def get_polygons_in_bucket_surrounding(self, x, y):
+        polygons_out = []
+        for x_i in range(x-1, x+2):
+            for y_i in range(y-1, y+2):
+                polygons_out += self.get_polygons_in_bucket(x_i, y_i)
+        return polygons_out
 
-    def _create_hashtable_from_polygons(polygons):
-        pass
+    def get_polygons_in_bucket(self, x, y):
+        return self.hashtable[x][y]
 
-    def _write_hashtable_to_file():
+    def get_hash_keys_of_polygon(self, polygon):
+        center = geometry_utils.polygon_centroid(polygon)
+        x = (center[0]-top_left_x)/cell_size
+        y = (center[1]-top_left_y)/cell_size
+        return round(x), round(y)
+
+    def write_hashtable_to_file(self):
         with open('geometry-hashtable-' + name, 'wb') as fp:
-            pickle.dump(hashtable, fp)
+            pickle.dump(self.hashtable, fp)
 
-    def _load_hashtable_from_file():
+    def load_hashtable_from_file(self):
         with open ('geometry-hashtable-' + name, 'rb') as fp:
-            hashtable = pickle.load(fp)
+            self.hashtable = pickle.load(fp)
 

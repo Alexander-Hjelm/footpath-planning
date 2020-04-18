aca4d74f3e676d3edcbab8f4e344632306319c89
Alexander Hjelm <nick.nertova@gmail.com>
2020-04-18 14:50:30 +0200

    Add helper function for line-line intersection

--- osm-importer/geometry_utils.py
+++ osm-importer/geometry_utils.py
@@ -110,6 +110,22 @@ def polygon_line_intersection(polygon, line_point_1, line_point_2):
         return False
     return True
 
+def line_line_intersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+       return None
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return x, y
+
 def polygon_centroid(polygon):
     centroid = [0.0, 0.0]
     counted_points = 0

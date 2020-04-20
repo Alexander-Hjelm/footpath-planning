59fb3370ab6e094f2c12fef1abeef9e3ee8ec4d2
Alexander Hjelm <nick.nertova@gmail.com>
2020-04-20 14:39:06 +0200

    Print post-correction statistics

--- osm-importer/way-collisions.py
+++ osm-importer/way-collisions.py
@@ -260,16 +260,24 @@ statistics_dict['stat_collision_node_count'] = stat_collision_node_count
 statistics_dict['stat_total_node_count'] = stat_total_node_count
 statistics_dict['stat_collision_edge_len'] = stat_collision_edge_len
 statistics_dict['stat_total_edge_len'] = stat_total_edge_len
+statistics_dict['stat_corrected_collision_feature_count'] = stat_corrected_collision_feature_count
+statistics_dict['stat_corrected_total_feature_count'] = stat_corrected_total_features_count
+statistics_dict['stat_corrected_collision_node_count'] = stat_corrected_collision_node_count
+statistics_dict['stat_corrected_total_node_count'] = stat_corrected_total_node_count
+statistics_dict['stat_corrected_collision_edge_len'] = stat_corrected_collision_edge_len
+statistics_dict['stat_corrected_total_edge_len'] = stat_corrected_total_edge_len
 
 with open('way-collision-statistics', 'wb') as fp:
     pickle.dump(statistics_dict, fp)
 
 # Results
-    print("Results...")
+print("Results...")
 for hwy in highway_categories:
     print("****************")
     print(hwy)
     print("****************")
+    print("Pre correction")
+    print("----------------")
     print("Colliding features count: " + str(stat_collision_feature_count[hwy]))
     print("Colliding nodes count: " + str(stat_collision_node_count[hwy]))
     print("Colliding edges cumulative length: " + str(stat_collision_edge_len[hwy]))
@@ -281,4 +289,19 @@ for hwy in highway_categories:
     print("% features count: " + str(stat_collision_feature_count[hwy]/stat_total_features_count[hwy]))
     print("% nodes count: " + str(stat_collision_node_count[hwy]/stat_total_node_count[hwy]))
     print("% edges cumulative length: " + str(stat_collision_edge_len[hwy]/stat_total_edge_len[hwy]))
+
+    print("----------------")
+    print("Post correction")
+    print("----------------")
+    print("Colliding features count: " + str(stat_corrected_collision_feature_count[hwy]))
+    print("Colliding nodes count: " + str(stat_corrected_collision_node_count[hwy]))
+    print("Colliding edges cumulative length: " + str(stat_corrected_collision_edge_len[hwy]))
+
+    print("Total features count: " + str(stat_corrected_total_features_count[hwy]))
+    print("Total nodes count: " + str(stat_corrected_total_node_count[hwy]))
+    print("Total edges cumulative length: " + str(stat_corrected_total_edge_len[hwy]))
+
+    print("% features count: " + str(stat_corrected_collision_feature_count[hwy]/stat_corrected_total_features_count[hwy]))
+    print("% nodes count: " + str(stat_corrected_collision_node_count[hwy]/stat_corrected_total_node_count[hwy]))
+    print("% edges cumulative length: " + str(stat_corrected_collision_edge_len[hwy]/stat_corrected_total_edge_len[hwy]))
 

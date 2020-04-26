a94e51d65fb7b45b0cf7683628cec0fd64f1697b
Alexander Hjelm <nick.nertova@gmail.com>
2020-04-26 15:09:50 +0200

    Plot and print polygons when recaping statistics

--- osm-importer/way-statistics-recap.py
+++ osm-importer/way-statistics-recap.py
@@ -1,6 +1,7 @@
 import pickle
 import statistics
 import plot_utils
+import geometry_utils
 
 highway_categories = [
         'footpath',
@@ -57,3 +58,33 @@ for hwy in highway_categories:
     print("% nodes count: " + str(stat_corrected_collision_node_count[hwy]/stat_total_node_count[hwy]))
     print("% edges cumulative length: " + str(stat_corrected_collision_edge_len[hwy]/stat_total_edge_len[hwy]))
 
+print(len(stat_colliding_features_before_correction))
+print(len(stat_colliding_features_after_correction))
+
+for id_1 in stat_colliding_features_before_correction.keys():
+    for id_2 in stat_colliding_features_before_correction[id_1].keys():
+        if id_1 in stat_colliding_features_after_correction.keys():
+            if id_2 in stat_colliding_features_after_correction[id_1].keys():
+                entry_before = stat_colliding_features_before_correction[id_1][id_2]
+                entry_after = stat_colliding_features_after_correction[id_1][id_2]
+                feature_1 = entry_before[0]
+                feature_2 = entry_before[1]
+                polygon_1_before = entry_before[2]
+                polygon_2_before = entry_before[3]
+                polygon_1_after = entry_after[2]
+                polygon_2_after = entry_after[3]
+
+                print("*****")
+                print("Polygon 1")
+                print(polygon_1_before)
+                print(polygon_1_after)
+                print("Polygon 2")
+                print(polygon_2_before)
+                print(polygon_2_after)
+
+
+                # Plot colliding features
+                if 'highway' in feature_2['properties']:
+                    plot_utils.plot_edges([polygon_2_before, polygon_2_after, polygon_1_before, polygon_1_after])
+                else:
+                    plot_utils.plot_polygons_and_paths([polygon_2_before, polygon_2_after], [polygon_1_before, polygon_1_after])

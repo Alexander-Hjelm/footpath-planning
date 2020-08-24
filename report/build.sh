da2cb88125a8a5ad686a073e2dbd850f414fb40f
Alexander Hjelm <nick.nertova@gmail.com>
2020-08-24 11:18:24 +0200

    Modify build script so there's an empty page between the front page and the title

--- report/build.sh
+++ report/build.sh
@@ -1,1 +1,1 @@
-pdflatex report.tex; pdftk cover_front.pdf report.pdf cover_rear.pdf output report_out.pdf; mv report_out.pdf report.pdf
+pdflatex report.tex; pdftk cover_front.pdf empty_page.pdf report.pdf cover_rear.pdf output report_out.pdf; mv report_out.pdf report.pdf
